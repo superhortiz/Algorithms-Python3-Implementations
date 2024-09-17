a693b46ff039f76f176314dd3e6ba237d408070a
Hector Ortiz <superhortiz@gmail.com>
2024-09-17 10:49:35 +0300

    Update shuffle.py

--- Algorithms/Others/shuffle.py
+++ Algorithms/Others/shuffle.py
@@ -14,16 +14,21 @@ def shuffle(a):
 
     for i in range(n):
         # Generate a random index from 0 to i
-        r = random.randint(0, i)
+        random_index = random.randint(0, i)
 
         # Swap the current element with the element at the random index
-        a[i], a[r] = a[r], a[i]
+        a[i], a[random_index] = a[random_index], a[i]
 
-
-# Example usage
-if __name__ == "__main__":
+def main():
+    """
+    Example usage
+    """
     a = [i for i in range(10)]
     print("Original array:  ", a)
 
     shuffle(a)
-    print("Knuth shuffle:   ", a)
+    print("Knuth shuffle:   ", a)  
+
+
+if __name__ == "__main__":
+    main()

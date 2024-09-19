b707b82048a5458997f2929a51e37a4ae31e72a7
Hector Ortiz <superhortiz@gmail.com>
2024-09-19 17:45:52 +0300

    Add files via upload

---
+++ algorithms/sorting/examples/example_dijkstra_3way_partition.py
@@ -0,0 +1,26 @@
+import random
+from algorithms.sorting.dijkstra_3way_partition import dijkstra_3way_partition
+
+def demo() -> None:
+    """
+    Example usage
+    """
+    help(dijkstra_3way_partition)
+    print("Example 1.")
+    print("Array before ordering:")
+    data = [random.randint(-20, 20) for _ in range(20)]
+    print(data)
+    print("Sorted Array in Ascending Order:")
+    dijkstra_3way_partition(data)
+    print(data)
+
+    print("\nExample 2.")
+    print("Array before ordering:")
+    data = [random.randint(-20, 20) for i in range(20)]
+    print(data)
+    print("Sorted Array in Ascending Order:")
+    dijkstra_3way_partition(data)
+    print(data)
+
+if __name__ == "__main__":
+    demo()
---
+++ algorithms/sorting/examples/example_insertion_sort.py
@@ -0,0 +1,26 @@
+import random
+from algorithms.sorting.insertion_sort import insertion_sort
+
+def demo() -> None:
+    """
+    Example usage
+    """
+    help(insertion_sort)
+    print("Example 1.")
+    print("Array before ordering:")
+    data = [random.randint(-20, 20) for _ in range(20)]
+    print(data)
+    print("Sorted Array in Ascending Order:")
+    insertion_sort(data)
+    print(data)
+
+    print("\nExample 2.")
+    print("Array before ordering:")
+    data = [random.randint(-20, 20) for i in range(20)]
+    print(data)
+    print("Sorted Array in Ascending Order:")
+    insertion_sort(data)
+    print(data)
+
+if __name__ == "__main__":
+    demo()

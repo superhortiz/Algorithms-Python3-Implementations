a5a98c30b546a61bc6be9eeddec23f59f6a2fe2c
Hector Ortiz <superhortiz@gmail.com>
2024-09-19 17:50:03 +0300

    Add files via upload

---
+++ algorithms/others/examples/example_quick_select.py
@@ -0,0 +1,31 @@
+import random
+from algorithms.others.quick_select import quick_select
+
+def demo():
+    """
+    Example usage
+    """
+    help(quick_select)
+    a = [10, 9, 6, 20, 7, 8, 13, 0, 15, 11, 19, 12, 1, 18, 4, 17, 2, 14, 16, 3, 5]
+    k = 5
+    print("Example 1.")
+    print("Array:", a)
+    value = quick_select(a, k)
+    print(f"{k}th smallest item =", value)
+
+    random.shuffle(a)
+    k = 7
+    print("\nExample 2.")
+    print("Array:", a)
+    value = quick_select(a, k)
+    print(f"{k}th smallest item =", value)
+
+    random.shuffle(a)
+    k = 12
+    print("\nExample 3.")
+    print("Array:", a)
+    value = quick_select(a, k)
+    print(f"{k}th smallest item =", value)
+
+if __name__ == "__main__":
+    demo()
